72a6127a56babdd48b8dd1f9f51c1f8eedf26471
Chaitanya Mittal <chtnnh@gmail.com>
2020-03-29 00:27:40 +0400

    Made minor changes to background music js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 let background_music = new Audio();
 background_music.src = "../resources/audio.mp3";
 background_music.play();
+console.log(
+  "This music has been taken from https://www.youtube.com/watch?v=WTsmIbNku5g and is the sole property of the owner. This site takes no ownership and is not liable towards any copyright infringements."
+);
